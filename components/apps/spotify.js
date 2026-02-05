300c4aad3a5eec400f40fb8f362ce5176d93ab7d
TPS <beltikasacarbell@gmail.com>
2026-02-05 11:48:20 +0700

    Fix iframe className in Spotify component

--- components/apps/spotify.js
+++ components/apps/spotify.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Spotify() {
     return (
-        <iframe src="https://open.spotify.com" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://open.spotify.com" frameBorder="0" title="Spotify" className="https://open.spotify.com"></iframe>
     )
 }
 
