2a4141cbc8ce72f174124270b0b2b64ee344627a
TPS <beltikasacarbell@gmail.com>
2026-02-18 17:44:36 +0700

    Update Meta.js

--- components/SEO/Meta.js
+++ components/SEO/Meta.js
@@ -5,7 +5,7 @@ export default function Meta() {
     return (
         <Head>
            /* Primary Meta Tags */
-            <title> - Cyber Security Researcher</title>
+            <title> - The Name's The Book</title>
             <meta charSet="utf-8" />
             <meta name="title" content="ley malam Portfolio - Computer Engineering Student" />
             <meta name="description"
