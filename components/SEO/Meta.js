e7d1b0068ee79420b9b92535adc4c95e1582f36b
TPS <beltikasacarbell@gmail.com>
2026-02-19 13:44:30 +0700

    Update meta tags for portfolio information

--- components/SEO/Meta.js
+++ components/SEO/Meta.js
@@ -5,14 +5,14 @@ export default function Meta() {
     return (
         <Head>
            /* Primary Meta Tags */
-            <title> - The Name’s The Book</title>
+            <title> - The Name’s The Book Is On The Table</title>
             <meta charSet="utf-8" />
-            <meta name="title" content="ley malam Portfolio - Computer Engineering Student" />
+            <meta name="title" content="hi people Portfolio - Computer Engineering Student" />
             <meta name="description"
-                content="ley malam's (x3rcyb) Personal Portfolio Website. Made with kali linux (Linux) theme by Next.js and Tailwind CSS." />
-            <meta name="author" content="ley malam (x3rcyb)" />
+                content="hi people's (x3rcyb) Personal Portfolio Website. Made with kali linux (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="hi people (x3rcyb)" />
             <meta name="keywords"
-                content="x3rcyb, x3rcyb's portfolio, x3rcyb linux, kali linux portfolio, ley malam protfolio,ley malam computer, ley malam, kali linux, ley malam kali linux portfolio" />
+                content="x3rcyb, x3rcyb's portfolio, x3rcyb linux, kali linux portfolio, hi people protfolio,hi people computer, hi people, kali linux, hi people kali linux portfolio" />
             <meta name="robots" content="index, follow" />
             <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
             <meta name="language" content="English" />
@@ -22,25 +22,25 @@ export default function Meta() {
             /* Search Engine */
             <meta name="image" content="images/logos/fevicon.png" />
             /* Schema.org for Google */
-            <meta itemProp="name" content="ley malam Portfolio - Computer Engineering Student" />
+            <meta itemProp="name" content="hi people Portfolio - Computer Engineering Student" />
             <meta itemProp="description"
-                content="ley malam's (x3rcyb) Personal Portfolio Website. Made with kali linux (Linux) theme by Next.js and Tailwind CSS." />
+                content="hi people's (x3rcyb) Personal Portfolio Website. Made with kali linux (Linux) theme by Next.js and Tailwind CSS." />
             <meta itemProp="image" content="images/logos/fevicon.png" />
             /* Twitter */
             <meta name="twitter:card" content="summary" />
-            <meta name="twitter:title" content="ley malam Portfolio - Computer Engineering Student" />
+            <meta name="twitter:title" content="hi people Portfolio - Computer Engineering Student" />
             <meta name="twitter:description"
-                content="ley malam's (x3rcyb) Personal Portfolio Website. Made with kali linux (Linux) theme by Next.js and Tailwind CSS." />
+                content="hi people's (x3rcyb) Personal Portfolio Website. Made with kali linux (Linux) theme by Next.js and Tailwind CSS." />
             <meta name="twitter:site" content="x3rcyb" />
             <meta name="twitter:creator" content="x3rcyb" />
             <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
             /* Open Graph general (Facebook, Pinterest & Google+) */
-            <meta name="og:title" content="ley malam Portfolio - Computer Engineering Student" />
+            <meta name="og:title" content="hi people Portfolio - Computer Engineering Student" />
             <meta name="og:description"
-                content="ley malam's (x3rcyb) Personal Portfolio Website. Made with kali linux (Linux) theme by Next.js and Tailwind CSS." />
+                content="hi people's (x3rcyb) Personal Portfolio Website. Made with kali linux (Linux) theme by Next.js and Tailwind CSS." />
             <meta name="og:image" content="images/logos/logo_1200.png" />
             <meta name="og:url" content="http://x3rcyb.github.io/" />
-            <meta name="og:site_name" content="ley malam Personal Portfolio" />
+            <meta name="og:site_name" content="hi people Personal Portfolio" />
             <meta name="og:locale" content="en_IN" />
             <meta name="og:type" content="website" />
 
