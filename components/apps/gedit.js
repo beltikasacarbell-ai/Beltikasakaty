9fee26652a1c876d0028dc8616a93c46deab854f
TPS <beltikasacarbell@gmail.com>
2026-03-20 20:50:17 +0700

    Update gedit.js

--- components/apps/gedit.js
+++ components/apps/gedit.js
@@ -80,7 +80,7 @@ export class Gedit extends Component {
                     <img src="/images/logos/medium.png" className="h-16 w-16"></img>
                     <span>medium</span>
                 </a>
-                <a href="https://www.tiktok.com/@Beltika_sakaty" className="flex flex-col h-max justify-center items-center" target="_blank">
+                <a href="https://www.tiktok.com/@beltikasakaty?lang=en-GB" className="flex flex-col h-max justify-center items-center" target="_blank">
                     <img src="/images/logos/Tiktok.jpg" className="h-16 w-16"></img>
                     <span>TikTok</span>
                 </a>
