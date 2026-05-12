d9e5b5a6aef4cf079ac2d17ae901430b65fc20cd
TPS <beltikasacarbell@gmail.com>
2026-05-12 12:09:31 +0700

    Add files via upload

---
+++ components/apps/burpsuite.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Burpsuite() {
+    return (
+        <iframe src="https://www.burpsuite.com frameBorder="0" title="Burpsuite" className="h-full w-full bg-ub-cool-grey"></iframe>
+    )
+}
+
+export const displayBurpsuite = () => {
+    <Burpsuite> </Burpsuite>
+}
