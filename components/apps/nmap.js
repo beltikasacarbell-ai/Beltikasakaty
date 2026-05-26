9f54677765aba2d22eae8767eab9a8deb6e9f1f4
TPS <beltikasacarbell@gmail.com>
2026-05-26 07:59:41 +0700

    Update nmap.js

--- components/apps/nmap.js
+++ components/apps/nmap.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Nmap() {
     return (
-        <iframe src="https://open.nmap.com/embed/playlist/37i9dQZEVXbLZ52XmnySJg" frameBorder="0" title=Nmap" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://open.nmap.com" frameBorder="0" title=Nmap" className="h-full w-full bg-ub-cool-grey"></iframe>
     )
 }
 
