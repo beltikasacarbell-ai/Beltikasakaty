7f03d82da6b30ce35d2a2db9b9cbcba9176b6b2c
TPS <beltikasacarbell@gmail.com>
2026-07-12 17:09:42 +0700

    Add files via upload

---
+++ components/apps/uiinterface.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Uiinterface() {
+    return (
+        <iframe src="https://www.ui.com" frameBorder="0" title="Ui interface" className="h-full w-full bg-ub-cool-grey"></iframe>
+    )
+}
+
+export const displayUiinterface = () => {
+    <Uiinterface> </Uiinterface>
+}
