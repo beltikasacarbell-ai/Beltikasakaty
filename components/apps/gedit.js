ccf264d7cd897929d5e3b68d9d32d4f5f8dc8e30
TPS <beltikasacarbell@gmail.com>
2026-07-12 16:58:32 +0700

    Update gedit.js

--- components/apps/gedit.js
+++ components/apps/gedit.js
@@ -68,7 +68,7 @@ export class Gedit extends Component {
     render() {
         return (
             <div className="w-full h-full relative flex gap-5 p-5  bg-[#282c34] text-white select-none ">
-                <a href="https://web.telegram.org/a/" className="flex flex-col h-max justify-center items-center" target="_blank">
+                <a href="https://t.me/malamley" className="flex flex-col h-max justify-center items-center" target="_blank">
                     <img src="/images/logos/telegram.png" className="h-16 w-16"></img>
                     <span classname="text-center">Telegram</span>
                 </a>
