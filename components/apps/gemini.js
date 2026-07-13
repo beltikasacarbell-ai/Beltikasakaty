f571c18eea7e3589100bdf4ae828d82e0e3eecc3
TPS <beltikasacarbell@gmail.com>
2026-07-13 20:54:25 +0700

    Add files via upload

---
+++ components/apps/gemini.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function gemini() {
+    return (
+        <iframe src="https://www.ui.com" frameBorder="0" title="Burpsuite" className="h-full w-full bg-ub-cool-grey"></iframe>
+    )
+}
+
+export const displayBurpsuite = () => {
+    <Burpsuite> </Burpsuite>
+}
