859c479c1ee1150726d3467586030e16c48da635
TPS <beltikasacarbell@gmail.com>
2026-07-14 10:11:22 +0700

    Update default.js

--- components/context menus/default.js
+++ components/context menus/default.js
@@ -16,7 +16,7 @@ function DefaultMenu(props) {
             <a rel="noreferrer noopener" href="https://github.com/x3rcyb" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
                 <span className="ml-5">🐈🤝</span> <span className="ml-2">Follow on <strong>Github</strong></span>
             </a>
-            <a rel="noreferrer noopener" href="mailto:x3rcyb@gmail.com" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
+            <a rel="noreferrer noopener" href="Beltikasacarbell@gmail.com" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
                 <span className="ml-5">👋📥</span> <span className="ml-2">Contact</span>
             </a>
             <Devider />
