5a8154857fab9aed02c613c2fd533e38f4b96d0f
TPS <beltikasacarbell@gmail.com>
2026-07-14 10:25:12 +0700

    Update default.js

--- components/context menus/default.js
+++ components/context menus/default.js
@@ -17,7 +17,7 @@ function DefaultMenu(props) {
                 <span className="ml-5">🐈🤝</span> <span className="ml-2">Follow on <strong>Github</strong></span>
             </a>
             <a rel="noreferrer noopener" href="mailto:x3rcyb@gmail.com" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
-                <span className="ml-5">👋📥</span> <span className="ml-2">Contact</span>
+                <span className="ml-5">👋📥</span> <span className="ml-2">Contact </span>
             </a>
             <Devider />
             <div onClick={() => { localStorage.clear(); window.location.reload() }} className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
