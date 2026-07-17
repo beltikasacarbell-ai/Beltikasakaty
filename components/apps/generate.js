af7d8bf2af523949436f317cfd138bb9338e93b9
TPS <beltikasacarbell@gmail.com>
2026-07-17 17:49:45 +0700

    Add files via upload

---
+++ components/apps/generate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function generate() {
+    return (
+        <iframe src="https://gemini.google.com/app" frameBorder="0" title="Gemini" className="h-full w-full bg-ub-cool-grey"></iframe>
+    )
+}
+
+export const displayGenerate = () => {
+    <Generate> </Generate>
+}
