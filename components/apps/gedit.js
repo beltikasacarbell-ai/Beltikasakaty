90c7973b26b54ad7115ee3810980d3ac7e797f4a
TPS <beltikasacarbell@gmail.com>
2026-07-29 23:58:47 +0700

    Update gedit.js

--- components/apps/gedit.js
+++ components/apps/gedit.js
@@ -72,7 +72,7 @@ export class Gedit extends Component {
                     <img src="/images/logos/faceboo.png" className="h-16 w-16"></img>
                     <span classname="text-center">Face</span>
                 </a>
-                <a href="https://www.facebook.com/share/17mQ4zU6th/?mibextid=wwXIfr" className="flex flex-col h-max justify-center items-center"  target="_blank">
+                <a href="Beltikasacarbell@gmail.com" className="flex flex-col h-max justify-center items-center"  target="_blank">
                     <img src="/images/logos/email.png" className="h-16 w-16"></img>
                     <span>mail</span>
                 </a>
